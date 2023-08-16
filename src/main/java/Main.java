80fcbf953160039b94f2f5ae7cf08515ff5406d7
Alexandr Strukov <alxstrukov@yandex.by>
2023-08-16 21:52:52 +0300

    add comment in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Maven");
-        System.out.println("Hello Maven");
+        System.out.println("Hello Maven2");
         /*при создании мавен проекта, в файле pom.xml необходимо подключить нужные зависимости (dependency) с которыми
         будет связан наш проект. Создать тег депенденси можно при помощи Alt+insert и вписать название, либо зайти на сайт
         мавен репозиторий и оттуда скопировать текст. Тоже самое и с плагинами*/
